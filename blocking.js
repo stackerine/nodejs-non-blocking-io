e1211ed9447bece9b8ce35d65fd726c014b58258
Joe <bazzooka@gmail.com>
2018-10-20 19:29:10 +0200

    add log

--- blocking.js
+++ blocking.js
@@ -1,6 +1,8 @@
 const getUserSync = require('./getUserSync');
 
+console.log('Get User 1');
 const userSync1 = getUserSync(321, console.log);
+console.log('Get User 2');
 const userSync2 = getUserSync(456, console.log);
 
 const sum = 3 + 1;
--- non-blocking.js
+++ non-blocking.js
@@ -1,6 +1,8 @@
 const getUser = require('./getUser');
 
+console.log('Get User 1');
 const userAsync1 = getUser(123, console.log);
+console.log('Get User 2');
 const userAsync2 = getUser(123, console.log);
 const sum = 3 + 1;
 console.log(`The sum is ${sum}`);
